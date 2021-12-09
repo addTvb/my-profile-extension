ecc165f2909f67a10f0cca4c20f2e1e90079b4a4
addTvb <arslan.web.dev@gmail.com>
2021-12-09 12:24:36 +0500

    ✨feat: copy buttons now works, changed theme, added new sections and projects

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-function copyText(element) {
-    let range, selection, worked;
+const copyToClipboard = (str) => {
+	const el = document.createElement('textarea');
+	el.value = str;
+	el.setAttribute('readonly', '');
+	el.style.position = 'absolute';
+	el.style.left = '-9999px';
+	document.body.appendChild(el);
+	const selected =
+		document.getSelection().rangeCount > 0
+			? document.getSelection().getRangeAt(0)
+			: false;
+	el.select();
+	document.execCommand('copy');
+	document.body.removeChild(el);
+	if (selected) {
+		document.getSelection().removeAllRanges();
+		document.getSelection().addRange(selected);
+	}
+};
 
-    if (document.body.createTextRange) {
-        range = document.body.createTextRange();
-        range.moveToElementText(element);
-        range.select();
-    } else if (window.getSelection) {
-        selection = window.getSelection();
-        range = document.createRange();
-        range.selectNodeContents(element);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
+let buttonLn = document.querySelector('#button-ln');
+let buttonBlog = document.querySelector('#button-blog');
+let buttonHabr = document.querySelector('#button-habr');
+let buttonMedium = document.querySelector('#button-medium');
+let buttonGithub = document.querySelector('#button-github');
 
-    try {
-        document.execCommand("copy");
-        updateStatus("Copied address", "info");
-    } catch (err) {
-        alert("Unable to copy the address");
-    }
-}
-
-let button = document.querySelector("#button");
-
-button.addEventListener("click", () => copyText(document.querySelector("#input")));
+buttonLn.addEventListener('click', () => copyToClipboard('https://www.linkedin.com/in/arslan-abayev-addtvb/'));
+buttonBlog.addEventListener('click', () => copyToClipboard('https://addtvb-code-notes.netlify.app/'));
+buttonHabr.addEventListener('click', () => copyToClipboard('https://career.habr.com/arslan-abaev'));
+buttonMedium.addEventListener('click', () => copyToClipboard('https://abayew-arslan.medium.com/'));
+buttonGithub.addEventListener('click', () => copyToClipboard('https://github.com/addTvb'));
